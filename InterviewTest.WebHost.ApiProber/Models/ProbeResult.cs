042b2f6f36a4b7c16f8482989cc79f9c30223857
ash.blade <ashen.blade.dev@gmail.com>
2022-10-13 12:24:30 +0300

    add inner exception for ProbeResult

--- InterviewTest.WebHost.ApiProber/Models/ProbeResult.cs
+++ InterviewTest.WebHost.ApiProber/Models/ProbeResult.cs
@@ -2,18 +2,20 @@ namespace InterviewTest.WebHost.ApiProber.Models;
 
 public class ProbeResult
 {
+    public Exception? Exception { get; set; }
     public bool Succeed { get; }
     public string? ErrorMessage { get; }
 
-    private ProbeResult(bool success, string? errorMessage = null)
+    private ProbeResult(bool success, string? errorMessage = null, Exception? exception = null)
     {
         Succeed = success;
+        Exception = exception;
         ErrorMessage = errorMessage;
     }
 
     public static ProbeResult Success() => new(true);
-    public static ProbeResult Fail(string errorMessage) 
-        => new(false, errorMessage ?? throw new ArgumentNullException(nameof(errorMessage), ErrorMessageNull));
+    public static ProbeResult Fail(string errorMessage, Exception? exception = null) 
+        => new(false, errorMessage ?? throw new ArgumentNullException(nameof(errorMessage), ErrorMessageNull), exception);
 
     private const string ErrorMessageNull = "Error message must be provided when result is not successful";
 
